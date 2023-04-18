05fe8a727343c6c696207a98451fd793dd1d7a06
dan.vsm <dan.vsm@gmail.com>
2023-04-18 19:30:42 -0300

    test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const robotron = document.querySelector("#robotron")
+/*const robotron = document.querySelector("#robotron")
 
 robotron.addEventListener("click",  (evento) => {
     console.log(evento)
@@ -10,4 +10,98 @@ function dizOi(nome) {
 }
 
 
-dizOi("Daniel")
+dizOi("Daniel") */
+
+
+
+const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+console.log(estatisticas)
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+
+controle.forEach ( (elemento) => {
+    elemento.addEventListener("click", (e) => 
+    {
+        manipulaDados(e.target.dataset.controle, e.target.parentNode)
+        atualizaEstatisticas(e.target.dataset.peca, e.target.dataset.controle)
+    })
+})
+
+
+
+
+
+function manipulaDados(operacao, controle, peca) {
+    const peca = controle.querySelector('[data-contador]')
+    
+    if (operacao === "-") {
+        if (peca.value > 0) {
+            peca.value = parseInt(peca.value) - 1
+        }    
+        
+            
+    
+    } else {
+            peca.value = parseInt(peca.value) + 1
+            
+    }
+ }
+
+
+ 
+function atualizaEstatisticas(peca, control, conte) {
+   const controles = control
+    estatisticas.forEach((elemento) => {
+    if (controles === "+"){
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    } else {
+        console.log(elemento.dataset.estatistica)
+        if (conte >= 0) {
+            elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica]
+        }
+        
+    }
+
+            
+            
+        
+    
+})
+
+
+
+}
